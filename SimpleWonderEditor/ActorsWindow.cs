d269165342e28465c738ff71d189fc34c2bd5018
CharlesDev <charlesgamedev@gmail.com>
2024-02-03 02:52:14 -0500

    base commit

---
+++ SimpleWonderEditor/ActorsWindow.cs
@@ -0,0 +1,152 @@
+﻿using ImGuiNET;
+using System.Reflection;
+
+namespace SimpleWonderEditor
+{
+    static class ActorsWindow
+    {
+        class ActorEntry
+        {
+            public required string Name;
+            public string ActorName = "";
+            public required List<ActorEntry> Children;
+            public bool Show;
+        }
+
+        private static bool _initialized;
+        private static string _allActorSearch = "";
+        private static int _allActorMatches = 0;
+        private static List<ActorEntry> actorEntries = [];
+        private static int _totalActorEntries;
+        private static bool _includeActorNameInSearch = true;
+
+        public static void Initialize()
+        {
+            if (_initialized) return;
+
+            Print("init actor list");
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "actors.txt";
+
+            using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new StreamReader(stream);
+            List<string> result = reader.ReadToEnd().Split("\n").ToList();
+
+            ActorEntry? current = null;
+
+            int i = 0;
+            while (i < result.Count)
+            {
+                string str = result[i].Trim().Replace("\n", "").Replace("\r", "");
+                if (string.IsNullOrWhiteSpace(str) && current != null)
+                {
+                    actorEntries.Add(current);
+                    current = null;
+                    i++;
+                    continue;
+                }
+                if (!str.Contains('-'))
+                {
+                    current = new ActorEntry()
+                    {
+                        Name = str,
+                        Children = []
+                    };
+                } else if (current != null)
+                {
+                    string[] split = str.Split('-');
+                    current.Children.Add(new ActorEntry()
+                    {
+                        Name = split[1].Trim(),
+                        ActorName = split[0].Trim().Replace(" ", ""),
+                        Children = []
+                    });
+                    _totalActorEntries++;
+                }
+
+                i++;
+            }
+
+            _initialized = true;
+        }
+
+        public static void DrawGUI()
+        {
+            ImGui.Begin("Actors");
+
+            ImGui.BeginTabBar("Selection", ImGuiTabBarFlags.NoCloseWithMiddleMouseButton);
+
+            if (ImGui.BeginTabItem("Property Manager"))
+            {
+                ImGui.Text("No actor selected.");
+                ImGui.EndTabItem();
+            }
+
+            if (ImGui.BeginTabItem("All Actors"))
+            {
+                if (!_initialized)
+                {
+                    ImGui.Text("Initializing actor list...");
+                    if (!_initialized) Initialize();
+                } else
+                {
+                    ImGui.AlignTextToFramePadding();
+                    ImGui.Text("Search");
+                    ImGui.SameLine();
+                    ImGui.Checkbox("Include Actor Name", ref _includeActorNameInSearch);
+                    ImGui.SameLine();
+                    ImGui.InputText($"{_totalActorEntries} actors", ref _allActorSearch, 100);
+                    ImGui.Separator();
+
+                    _allActorSearch = _allActorSearch.Trim();
+
+                    foreach (var item in actorEntries)
+                        AddActorEntry(item);
+
+                    ImGui.EndTabItem();
+
+                    _allActorMatches = 0;
+                }
+            }
+
+            ImGui.EndTabBar();
+
+            ImGui.End();
+        }
+
+        static void AddActorEntry(ActorEntry item)
+        {
+            bool show;
+            if (item.Children.Count == 0)
+            {
+                if (item.Show && (item.Name.Contains(_allActorSearch, StringComparison.CurrentCultureIgnoreCase) || (_includeActorNameInSearch && item.ActorName.Contains(_allActorSearch, StringComparison.CurrentCultureIgnoreCase))))
+                    ActorButton(item.ActorName, item.Name);
+                return;
+            } else
+            {
+                show = ImGui.CollapsingHeader(item.Name, ImGuiTreeNodeFlags.Leaf);
+            }
+
+            foreach (var child in item.Children)
+            {
+                child.Show = show;
+                AddActorEntry(child);
+            }
+        }
+
+        static void ActorButton(string actorName, string name)
+        {
+            ImGui.Button("+");
+            ImGui.SameLine();
+            ImGui.Text(name);
+            ImGui.SameLine();
+            ImGui.TextDisabled(actorName);
+            _allActorMatches++;
+        }
+
+        static void Print(object msg)
+        {
+            Console.WriteLine($"[ActorsWindow]: {msg}");
+        }
+    }
+}
---
+++ SimpleWonderEditor/Program.cs
@@ -0,0 +1,134 @@
+﻿using ImGuiNET;
+using System.Diagnostics;
+using System.Numerics;
+using System.Reflection;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace SimpleWonderEditor
+{
+    class Program
+    {
+        private static Sdl2Window _window;
+        private static GraphicsDevice _gd;
+        private static CommandList _cl;
+        private static ImGuiController _controller;
+
+        private static bool _showActorWindow = true;
+
+        private static Assembly assembly = Assembly.GetExecutingAssembly();
+
+        private static Vector3 _clearColor = new Vector3(0.45f, 0.55f, 0.6f);
+
+        static void Main(string[] args)
+        {
+            Print("init window");
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(0, 0, 1280, 720, WindowState.Maximized, "Simple Wonder Editor"),
+                new GraphicsDeviceOptions(true, null, false, ResourceBindingModel.Improved, true, true),
+                out _window,
+                out _gd);
+            _window.Resized += () =>
+            {
+                _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+                _controller.WindowResized(_window.Width, _window.Height);
+            };
+            _cl = _gd.ResourceFactory.CreateCommandList();
+            Print("init imGui");
+            _controller = new ImGuiController(_gd, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+
+            Print("main loop");
+            var stopwatch = Stopwatch.StartNew();
+            float deltaTime = 0f;
+            // Main application loop
+            while (_window.Exists)
+            {
+                deltaTime = stopwatch.ElapsedTicks / (float)Stopwatch.Frequency;
+                stopwatch.Restart();
+                InputSnapshot snapshot = _window.PumpEvents();
+                if (!_window.Exists) { break; }
+                _controller.Update(deltaTime, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                SubmitUI();
+
+                _cl.Begin();
+                _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+                _cl.ClearColorTarget(0, new RgbaFloat(_clearColor.X, _clearColor.Y, _clearColor.Z, 1f));
+                _controller.Render(_gd, _cl);
+                _cl.End();
+                _gd.SubmitCommands(_cl);
+                _gd.SwapBuffers(_gd.MainSwapchain);
+            }
+
+            Print("exiting");
+            // Clean up Veldrid resources
+            Exit();
+        }
+
+        static void Print(object msg)
+        {
+            Console.WriteLine($"[SWE]: {msg}");
+        }
+
+        static void Exit()
+        {
+            _gd.WaitForIdle();
+            _controller.Dispose();
+            _cl.Dispose();
+            _gd.Dispose();
+            Environment.Exit(0);
+        }
+
+        private static unsafe void SubmitUI()
+        {
+            ImGui.BeginMainMenuBar();
+
+            if (ImGui.BeginMenu("File"))
+            {
+                if (ImGui.MenuItem("New...", "Ctrl+N"))
+                {
+
+                }
+                if (ImGui.MenuItem("Open...", "Ctrl+O"))
+                {
+
+                }
+
+                ImGui.Separator();
+
+                if (ImGui.MenuItem("Exit"))
+                {
+                    Exit();
+                    return;
+                }
+
+                ImGui.EndMenu();
+            }
+
+            if (ImGui.BeginMenu("Edit"))
+            {
+                ImGui.MenuItem("Preferences", "Ctrl+P");
+
+                ImGui.EndMenu();
+            }
+
+            if (ImGui.BeginMenu("Window"))
+            {
+                ImGui.Checkbox("Actors", ref _showActorWindow);
+
+                ImGui.EndMenu();
+            }
+
+            ImGui.EndMainMenuBar();
+
+            if (_showActorWindow)
+                ActorsWindow.DrawGUI();
+        }
+
+        private static void OpenFile(string path)
+        {
+            Print(path);
+        }
+    }
+}
